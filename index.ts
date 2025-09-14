e9bcb3f122f554f687a8170a0908addddca4c37b
nkfr26 <n@rfu.jp>
2025-09-14 10:14:08 +0900

    refactor

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import { mkdirSync, writeFileSync } from "node:fs";
-import { parse } from "csv-parse/sync";
 
-function formatDate(date: Date) {
-  return date
+const formatDate = (date: Date) =>
+  date
     .toLocaleDateString("ja-JP", {
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
     })
     .replaceAll("/", "-");
-}
 
 (async () => {
   const response = await fetch(
@@ -19,25 +17,17 @@ function formatDate(date: Date) {
 
   const decoder = new TextDecoder("shift-jis");
   const csvText = decoder.decode(arrayBuffer);
-  const records: Record<string, string>[] = parse(csvText, {
-    columns: true,
-  });
 
-  const syukujitsu: Record<string, string> = {};
   const currentYear = new Date().getFullYear();
   const targetYears = [currentYear.toString(), (currentYear + 1).toString()];
 
-  for (const record of records) {
-    // 国民の祝日・休日月日,国民の祝日・休日名称
-    const [dateKey, nameKey] = Object.keys(record);
-
-    if (!dateKey || !nameKey) continue;
-    const date = record[dateKey];
-    const name = record[nameKey];
-    if (date && name && targetYears.some((year) => date.startsWith(year))) {
-      syukujitsu[formatDate(new Date(date))] = name;
-    }
-  }
+  const syukujitsu = Object.fromEntries(
+    csvText
+      .split("\r")
+      .map((line) => line.split(","))
+      .map(([date, name]) => [formatDate(new Date(date || "")), name])
+      .filter(([date]) => targetYears.some((year) => date?.startsWith(year))),
+  );
 
   mkdirSync("docs", { recursive: true });
   writeFileSync("docs/syukujitsu.json", JSON.stringify(syukujitsu, null, 2));
